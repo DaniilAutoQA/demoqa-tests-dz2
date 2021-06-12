c42dafa168c779f328720a3b3a0d1f1857b69c78
natal <svetnata74@mail.ru>
2021-06-12 07:30:17 +0200

    add test ToTry

---
+++ src/test/java/TryGit.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class TryGit {
+
+    @Test
+    void ToTry() {
+        System.out.println("Hallo");
+        System.out.println("12.06.2021");
+        System.out.println("Bye");
+    }
+}
