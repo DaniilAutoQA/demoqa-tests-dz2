118e434e89f6b1c41c83c25ebbd1ba9f671cfa71
natal <svetnata74@mail.ru>
2021-06-12 08:26:16 +0200

    third commit

--- src/test/java/TryGit.java
+++ src/test/java/TryGit.java
@@ -6,6 +6,7 @@ public class TryGit {
     void ToTry() {
         System.out.println("Hallo Nata");
         System.out.println("12.06.2021 Hi hi hi");
+        System.out.println("12.06.2021");
         System.out.println("Bye");
     }
 }
