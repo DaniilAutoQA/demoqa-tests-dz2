380ff78a5ba7cab8496d44a113a2c22745ecd7f5
natal <svetnata74@mail.ru>
2021-06-12 10:23:12 +0200

    2

--- src/test/java/TryGit.java
+++ src/test/java/TryGit.java
@@ -5,7 +5,7 @@ public class TryGit {
     @Test
     void ToTry() {
         System.out.println("Hallo Nata");
-        System.out.println("12.06.2021");
-        System.out.println("Bye");
+        System.out.println("12.06.2021 hi hi");
+        System.out.println("Bye bye bye");
     }
 }
