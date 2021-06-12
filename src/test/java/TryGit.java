52a0b7049523794c547363c72b2a07564f82d20b
natal <svetnata74@mail.ru>
2021-06-12 09:57:52 +0200

    first commit

--- src/test/java/TryGit.java
+++ src/test/java/TryGit.java
@@ -4,7 +4,7 @@ public class TryGit {
 
     @Test
     void ToTry() {
-        System.out.println("Hallo");
+        System.out.println("Hallo Nata");
         System.out.println("12.06.2021");
         System.out.println("Bye");
     }
