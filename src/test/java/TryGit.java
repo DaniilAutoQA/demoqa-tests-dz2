fe3b33de72c52c664f5df01205a90a4ebbd93f2d
DaniilAutoQA <daniil-bond@yandex.ru>
2021-06-25 15:45:18 +0300

    com1

--- src/test/java/TryGit.java
+++ src/test/java/TryGit.java
@@ -5,7 +5,7 @@ public class TryGit {
     @Test
     void ToTry() {
         System.out.println("Hallo");
-        System.out.println("12.06.2021");
+        System.out.println("12.06.2
         System.out.println("Bye");
         System.out.println("Hi Natali, I'm Daniil");
     }
